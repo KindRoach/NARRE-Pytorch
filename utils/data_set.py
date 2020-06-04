e69fd23a1e368db559800cf2eb8addf61d6dcb6e
kindroach <kindroach@hotmail.com>
2020-06-04 21:46:41 +0800

    Add note to data_set.py

--- utils/data_set.py
+++ utils/data_set.py
@@ -14,13 +14,21 @@ class NarreDateset(Dataset):
     def __init__(self, data: DataFrame,
                  user_review: Dict[str, DataFrame],
                  item_review: Dict[str, DataFrame],
-                 max_count: int, review_length: int):
+                 review_count: int, review_length: int):
+        """
+        Init a NarreDateset.
+        :param data: original data. ["userID","itemID","review","rating"]
+        :param user_review: the review grouped by userID
+        :param item_review: the review grouped by itemID
+        :param review_count: the max count of review per user/item
+        :param review_length: the length of review
+        """
 
         super().__init__()
         self.data = data
         self.user_review = user_review
         self.item_review = item_review
-        self.max_count = max_count
+        self.review_count = review_count
         self.review_length = review_length
 
         logger.info("Loading dataset...")
@@ -52,7 +60,7 @@ class NarreDateset(Dataset):
         """
         1. Load review from review dict by userID/itemID
         2. Exclude unknown review by itemID/userID.
-        3. Pad review text to max_count
+        3. Pad or shorten review text to max_count
 
         E.g. get all reviews written by user1 except itemA
              when we predict the rating of itemA marked by user1.
@@ -79,10 +87,10 @@ class NarreDateset(Dataset):
         reviews = reviews["review"][reviews[key] != exclude_id].to_list()
         reviews = [r[:self.review_length] for r in reviews]
 
-        if len(reviews) >= self.max_count:
-            reviews = reviews[:self.max_count]
+        if len(reviews) >= self.review_count:
+            reviews = reviews[:self.review_count]
         else:
-            reviews += [[PAD_WORD_ID] * self.review_length] * (self.max_count - len(reviews))
+            reviews += [[PAD_WORD_ID] * self.review_length] * (self.review_count - len(reviews))
         return reviews
 
 
