928156e87189e582d5273a3c633021b65d2470dd
kindroach <kindroach@hotmail.com>
2020-06-06 16:31:48 +0800

    Use independent id embedding for quality and preference id.

--- model/narre.py
+++ model/narre.py
@@ -28,9 +28,12 @@ class NarreConfig(BaseConfig):
 
 
 class ReviewEncoder(torch.nn.Module):
-    def __init__(self, config: NarreConfig):
+    def __init__(self, config: NarreConfig, preference_id_count: int, quality_id_count: int):
         super().__init__()
         self.config = config
+
+        self.preference_id_embedding = torch.nn.Embedding(preference_id_count, config.id_dim)
+        self.quality_id_embedding = torch.nn.Embedding(quality_id_count, config.id_dim)
 
         self.conv = torch.nn.Conv1d(
             in_channels=config.word_dim,
@@ -48,31 +51,36 @@ class ReviewEncoder(torch.nn.Module):
         self.top_linear = torch.nn.Linear(config.kernel_deep, config.id_dim)
         self.dropout = torch.nn.Dropout(self.config.dropout)
 
-    def forward(self, review, id_emb):
+    def forward(self, review_emb, preference_id, quality_id):
         """
         Input Size:
             (Batch Size, Review Count, Review Length, Word Dim)
-            (Batch Size, Review Count, Id Dim)
+            (Batch Size, Review Count)
+            (Batch Size, Review Count)
 
         Output Size:
-            (Batch Size, Id Dim)
+            (Batch Size, Id Dim * 2)
         """
 
-        batch_size = review.shape[0]
-        review_in_one = review.view(-1, self.config.review_length, self.config.word_dim)
+        preference_id_emb = self.preference_id_embedding(preference_id).view(-1, self.config.id_dim)
+        quality_id_emb = self.quality_id_embedding(quality_id)
+
+        batch_size = review_emb.shape[0]
+        review_in_one = review_emb.view(-1, self.config.review_length, self.config.word_dim)
         review_in_one = review_in_one.permute(0, 2, 1)
         review_conv = F.relu(self.conv(review_in_one))
         review_conv = self.max_pool(review_conv).view(-1, self.config.kernel_deep)
         review_in_many = review_conv.view(batch_size, self.config.review_count, -1)
 
         review_att = self.att_review(review_in_many)
-        id_att = self.att_id(id_emb)
+        id_att = self.att_id(quality_id_emb)
         att_weight = self.att_layer(F.relu(review_att + id_att))
         att_weight = F.softmax(att_weight, dim=1)
         att_out = (att_weight * review_in_many).sum(1)
 
         feature = self.dropout(att_out)
         feature = self.top_linear(feature)
+        feature = torch.cat([preference_id_emb, feature], dim=1)
         return feature
 
 
@@ -107,11 +115,8 @@ class NarreModel(BaseModel):
         self.word_embedding = torch.nn.Embedding.from_pretrained(word_embedding_weight)
         self.word_embedding.weight.requires_grad = False
 
-        self.user_embedding = torch.nn.Embedding(config.user_count, config.id_dim)
-        self.item_embedding = torch.nn.Embedding(config.item_count, config.id_dim)
-
-        self.user_review_layer = ReviewEncoder(config)
-        self.item_review_layer = ReviewEncoder(config)
+        self.user_review_layer = ReviewEncoder(config, config.user_count, config.item_count)
+        self.item_review_layer = ReviewEncoder(config, config.item_count, config.user_count)
 
         self.predict_linear = LatentFactor(config)
 
@@ -129,17 +134,11 @@ class NarreModel(BaseModel):
             (Batch Size, 1)
         """
 
-        user_review = self.word_embedding(user_review)
-        item_id_per_review_emb = self.item_embedding(item_id_per_review)
-        user_review = self.user_review_layer(user_review, item_id_per_review_emb)
-        user_id_emb = self.user_embedding(user_id).view(-1, self.config.id_dim)
-        user_feature = torch.cat([user_id_emb, user_review], dim=1)
+        user_review_emb = self.word_embedding(user_review)
+        user_feature = self.user_review_layer(user_review_emb, user_id, item_id_per_review)
 
-        item_review = self.word_embedding(item_review)
-        user_id_per_review_emb = self.user_embedding(user_id_per_review)
-        item_review = self.item_review_layer(item_review, user_id_per_review_emb)
-        item_id_emb = self.item_embedding(item_id).view(-1, self.config.id_dim)
-        item_feature = torch.cat([item_id_emb, item_review], dim=1)
+        item_review_emb = self.word_embedding(item_review)
+        item_feature = self.item_review_layer(item_review_emb, item_id, user_id_per_review)
 
         predict = self.predict_linear(user_feature, user_id, item_feature, item_id)
         return predict
